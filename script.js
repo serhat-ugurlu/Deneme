e51e21e64d4769afe4fc9d57de547b6ae26f555e
serhat-ugurlu <serhatomerugurlu@etu.edu.tr>
2025-11-17 22:52:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('princessButton').addEventListener('click', function() {
     const message = document.getElementById('celebrationMessage');
 
     // Buton metnini değiştir (isteğe bağlı)
-    this.textContent = 'Şenlendik!';
+    this.textContent = 'Şenlendik';
     this.disabled = true;
 
     // Mesajı göster
